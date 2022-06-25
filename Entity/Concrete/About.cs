2d82a7538b205f4d517d741d41d588898c79e7ad
nesibecetin <nesibe3cetin@gmail.com>
2022-06-25 18:46:53 +0300

    initial commit

---
+++ Entity/Concrete/About.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Concrete
+{
+    public class About
+    {
+
+        public int Id { get; set; }
+        public string Details1 { get; set; }
+        public string Details2 { get; set; }
+        public string Image1v { get; set; }
+        public string Image2 { get; set; }
+        public string MapLocation { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ Entity/Concrete/Article.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Concrete
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string ThumbnailImage { get; set; }
+        public string Image { get; set; }
+        public string CreateDate { get; set; }
+        public bool Status { get; set; }
+    }
+}
