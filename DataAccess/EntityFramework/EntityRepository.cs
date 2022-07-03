ef620dabf1b16373ae485b4729701403ca82d984
nesibecetin <nesibecetin@morqet.com>
2022-07-03 22:36:07 +0300

    Generic Repository

---
+++ DataAccess/EntityFramework/EntityRepository.cs
@@ -0,0 +1,55 @@
+﻿using DataAccess.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public class EntityRepository<T> : IEntityDal<T> where T : class
+    {
+        public void Add(T entity)
+        {
+            using (var context = new Context())
+            {
+                context.Add(entity);
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(T entity)
+        {
+            using (var context = new Context())
+            {
+                context.Remove(entity);
+                context.SaveChanges();
+            }
+        }
+
+        public T Get(int id)
+        {
+            using (var context = new Context())
+            {
+                return context.Set<T>().Find(id);
+            }
+        }
+
+        public List<T> GetAll()
+        {
+            using (var context = new Context())
+            {
+               return context.Set<T>().ToList();
+            }
+        }
+
+        public void Update(T entity)
+        {
+            using (var context = new Context())
+            {
+                context.Update(entity);
+                context.SaveChanges();
+            }
+        }
+    }
+}
